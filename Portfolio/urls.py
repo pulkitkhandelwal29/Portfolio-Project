1982abce49e9965858ac974d6a012edd8ebb655b
pulkitkhandelwal29 <pulkit.khandelwal29@gmail.com>
2021-01-20 10:46:02 +0530

    Bootstrap added and Jobs showing up

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -8,6 +8,10 @@ from django.conf import settings
 #Helps to work with the static files
 from django.conf.urls.static import static
 
+#importing views from the models for homepage
+import jobs.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',jobs.views.home, name='home')
 ]+ static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+#Importing job class from models.py of Jobs app
+#It is called so that we can import what we have in database and render it to the html page for displaying
+from .models import Job
+
+def home(request):
+    #It is calling all the objects i.e. database from the Job class
+    jobs = Job.objects
+    return render(request,'jobs/home.html',{'jobs':jobs})
